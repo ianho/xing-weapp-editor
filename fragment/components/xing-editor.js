fed995c03b567a470f4547487564b86e87768ce1
Cribug <anho.zhang@gmail.com>
2018-08-09 17:09:27 +0800

    update editor

--- fragment/components/xing-editor.js
+++ fragment/components/xing-editor.js
@@ -22,6 +22,16 @@ Component({
       type: String,
       value: 'array',
     },
+
+    buttonBackgroundColor: {
+      type: String,
+      value: '#409EFF',
+    },
+
+    buttonTextColor: {
+      type: String,
+      value: '#fff',
+    },
   },
 
   /**
@@ -31,6 +41,8 @@ Component({
     windowHeight: 0,
     base64: false,
     nodes: [],
+    nodesData: [],
+    textareaFocus: null,
   },
 
   attached: function () {
@@ -49,6 +61,97 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    updateNodes: function (e) {
+      const nodesData = this.data.nodesData;
+      let nodes = this.data.nodes;
+      nodes.forEach((node, index) => {
+        if (node.name === 'p') {
+          node.children[0].text = nodesData[index];
+        }
+        // if (node.name === 'img') {
+        //   node.attrs.src = nodesData[index];
+        // }
+      })
+      this.setData({
+        nodes,
+      })
+    },
 
+    addText: function (e) {
+      this.updateNodes();
+      const index = e.currentTarget.dataset.index;
+      const node = {
+        name: 'p',
+        attrs: {
+          class: 'xing-p',
+        },
+        children: [{
+          type: 'text',
+          text: ''
+        }]
+      }
+      let nodes = this.data.nodes;
+      let nodesData = this.data.nodesData;
+      nodes.splice(index + 1, 0, node);
+      nodesData.splice(index + 1, 0, '');
+      this.setData({
+        nodes,
+        nodesData,
+        textareaFocus: index + 1,
+      })
+    },
+
+    addImage: function (e) {
+      this.updateNodes();
+      const index = e.currentTarget.dataset.index;
+      wx.chooseImage({
+        success: res => {
+          const tempFilePath = res.tempFilePaths[0];
+          wx.getImageInfo({
+            src: tempFilePath,
+            success: res => {
+              console.log(res);
+              const node = {
+                name: 'img',
+                attrs: {
+                  class: 'xing-img',
+                  src: tempFilePath,
+                  _height: res.height / res.width,
+                },
+              }
+              let nodes = this.data.nodes;
+              let nodesData = this.data.nodesData;
+              nodes.splice(index + 1, 0, node);
+              nodesData.splice(index + 1, 0, tempFilePath);
+              this.setData({
+                nodes,
+                nodesData,
+              })
+            }
+          })
+        },
+      })
+    },
+
+    deleteNode: function (e) {
+      const index = e.currentTarget.dataset.index;
+      let nodes = this.data.nodes;
+      let nodesData = this.data.nodesData;
+      nodes.splice(index, 1);
+      nodesData.splice(index, 1);
+      this.setData({
+        nodes,
+        nodesData,
+      })
+    },
+
+    onTextareaInput: function (e) {
+      const index = e.currentTarget.dataset.index;
+      let nodesData = this.data.nodesData;
+      nodesData[index] = e.detail.value;
+      this.setData({
+        nodesData,
+      })
+    },
   }
 })
